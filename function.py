a09274be40b08845278c36524013481215ad4369
gunhe17 <pers.gun17@gmail.com>
2024-02-10 21:43:11 +0900

    feature: patch_part_data()

--- function.py
+++ function.py
@@ -1,12 +1,11 @@
+from pptx import Presentation
+from pptx.util import Pt
 import json
 
-backgroud_image = "./data/background_img/img_no{n}"
 initial_presentation = "./data/table_format.pptx"
 
 start_book = ""
 start_chapter = ""
-
-import json
 
 def get_weekly_plan(
         *, 
@@ -23,37 +22,85 @@ def get_weekly_plan(
     with open(bible_shortest_names, 'r') as file:
         shortest_names = json.load(file)
 
-    reading_list = []
+    weekly_plan = []
     current_book = start_book
     current_chapter = int(start_chapter)
     
-    for day in range(7):
+    for _ in range(7):
         daily_start_book = current_book
         daily_start_chapter = current_chapter
         
         for _ in range(daily_reading):
-            if current_chapter < bible_chapters[current_book]:
-                current_chapter += 1
-            else: 
-                books = list(bible_chapters.keys())
-                current_book_index = books.index(current_book)
-                if current_book_index + 1 < len(books):
-                    current_book = books[current_book_index + 1]
-                    current_chapter = 1
+            if current_chapter <= bible_chapters[current_book]:
+                if current_chapter == bible_chapters[current_book]:
+                    books = list(bible_chapters.keys())
+                    current_book_index = books.index(current_book)
+                    if current_book_index + 1 < len(books):
+                        current_book = books[current_book_index + 1]
+                        current_chapter = 1
                 else:
-                    break
+                    current_chapter += 1
+            else:
+                break
 
         abbreviated_start_book = shortest_names.get(daily_start_book, daily_start_book)
         abbreviated_current_book = shortest_names.get(current_book, current_book)
         
         if abbreviated_start_book == abbreviated_current_book:
-            abbreviated_current_book = ""
-        
-        reading_range = f"{abbreviated_start_book} {daily_start_chapter} - {abbreviated_current_book} {current_chapter - 1 if current_chapter > 1 else bible_chapters[daily_start_book]}"
-        reading_list.append(reading_range)
+            reading_range = f"{abbreviated_start_book} {daily_start_chapter}장 - {current_chapter - 1}장"
+        else:
+            reading_range = f"{abbreviated_start_book} {daily_start_chapter}장 - {abbreviated_current_book} {current_chapter - 1}장"
+
+        weekly_plan.append(reading_range)
     
-    return reading_list
+    return weekly_plan
+
+def patch_cell(cell, text):
+    cell.text = ""
+    text_frame = cell.text_frame
+    p = text_frame.add_paragraph()
+    run = p.add_run()
+    run.text = text
+
+    run.font.name = "배달의민족 한나체 Pro"
+    run.font.size = Pt(46)
+
+def patch_part_data(
+        *,
+        date,
+        weekly_plan,
+        selected_custom,
+):
+    pptx = Presentation(f"./custom/{selected_custom}.pptx")
+    day_mapping = {
+        "part_sun": 0,
+        "part_mon": 1,
+        "part_tue": 2,
+        "part_wed": 3,
+        "part_thu": 4,
+        "part_fri": 5,
+        "part_sat": 6,
+    }
+
+    for slide in pptx.slides:
+        for shape in slide.shapes:
+            if shape.has_text_frame:
+                if "part_date" in shape.text:
+                    shape.text = shape.text.replace("part_date", date)
+
+                for day_key, plan_index in day_mapping.items():
+                    if day_key in shape.text:
+                        shape.text = shape.text.replace(day_key, weekly_plan[plan_index])
+
+    pptx.save("./patched_pptx.pptx")
+
+    return
+    
 
 
-weekly_reading = get_weekly_plan(start_book="에스더", start_chapter="1")
-print(weekly_reading)
+###############
+#   usecase
+###############
+
+weekly_plan = get_weekly_plan(start_book="창세기", start_chapter="1")
+patch_part_data(date="test_data", weekly_plan=weekly_plan, selected_custom="animation_fall")
--- readme.md
+++ readme.md
@@ -1,7 +1,11 @@
+# AUTO-WEEKLY-BIBLE-PLAN
+
+## Description
+`AUTO-WEEKLY-BIBLE-PLAN`은 성경 읽기 대행진 대회를 위해 매주 성경 읽기표를 자동으로 생성하는 프로젝트입니다. 
+
 ## to run
 
 source .venv/bin/activate
 
 pip install --upgrade pip
 pip install -r requirements.txt
-
