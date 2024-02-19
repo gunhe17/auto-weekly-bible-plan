32f127042f7a55756ebb70dbfcd103fef696d52d
gunhe17 <pers.gun17@gmail.com>
2024-02-19 17:55:56 +0900

    refactoring: get_weekly_plan()

--- function.py
+++ function.py
@@ -1,3 +1,4 @@
+from datetime import datetime, timedelta
 from pptx import Presentation
 from pptx.util import Pt
 import os
@@ -5,8 +6,13 @@ import json
 
 initial_presentation = "./data/table_format.pptx"
 
-start_book = ""
-start_chapter = ""
+
+def get_formatted_date(start_date_str):
+    start_date = datetime.strptime(start_date_str, "%y.%m.%d")
+    end_date = start_date + timedelta(days=6)
+    formatted_date_range = f"{start_date.month}월 {start_date.day}일 - {end_date.month}월 {end_date.day}일"
+    
+    return formatted_date_range
 
 def get_weekly_plan(
         *, 
@@ -16,45 +22,47 @@ def get_weekly_plan(
         bible_index="./data/bible_index.json", 
         bible_shortest_names="./data/bible_shortest_name.json"
     ):
-
+    
     with open(bible_index, 'r') as file:
         bible_chapters = json.load(file)
     
     with open(bible_shortest_names, 'r') as file:
         shortest_names = json.load(file)
-
+    
+    start_chapter = int(start_chapter)
     weekly_plan = []
     current_book = start_book
-    current_chapter = int(start_chapter)
-    
+    current_chapter = start_chapter
+
     for _ in range(7):
-        daily_start_book = current_book
-        daily_start_chapter = current_chapter
-        
-        for _ in range(daily_reading):
-            if current_chapter <= bible_chapters[current_book]:
-                if current_chapter == bible_chapters[current_book]:
-                    books = list(bible_chapters.keys())
-                    current_book_index = books.index(current_book)
-                    if current_book_index + 1 < len(books):
-                        current_book = books[current_book_index + 1]
-                        current_chapter = 1
-                else:
-                    current_chapter += 1
-            else:
-                break
-
-        abbreviated_start_book = shortest_names.get(daily_start_book, daily_start_book)
-        abbreviated_current_book = shortest_names.get(current_book, current_book)
-        
-        if abbreviated_start_book == abbreviated_current_book:
-            reading_range = f"{abbreviated_start_book} {daily_start_chapter}장 - {current_chapter - 1}장"
+        chapters_left = bible_chapters[current_book] - current_chapter + 1
+        if daily_reading <= chapters_left:
+            end_chapter = current_chapter + daily_reading - 1
+            reading_plan = f"{shortest_names[current_book]} {current_chapter} - {end_chapter}장"
+            current_chapter += daily_reading
         else:
-            reading_range = f"{abbreviated_start_book} {daily_start_chapter}장 - {abbreviated_current_book} {current_chapter - 1}장"
-
-        weekly_plan.append(reading_range)
+            next_book = next((book for book in bible_chapters if list(bible_chapters).index(book) > list(bible_chapters).index(current_book)), None)
+            
+            if next_book is None:
+                next_book = list(bible_chapters.keys())[0]
+                current_chapter = 1 + daily_reading - chapters_left
+                reading_plan = f"{shortest_names[current_book]} {current_chapter} - {shortest_names[next_book]} {current_chapter - 1}장"
+            else:
+                end_chapter = daily_reading - chapters_left
+                reading_plan = f"{shortest_names[current_book]} {current_chapter} - {shortest_names[next_book]} {end_chapter}장"
+                current_book = next_book
+                current_chapter = end_chapter + 1
+            
+        weekly_plan.append(reading_plan)
+        
+        if current_chapter > bible_chapters[current_book]:
+            current_book = next((book for book in bible_chapters if list(bible_chapters).index(book) > list(bible_chapters).index(current_book)), None)
+            if current_book is None:
+                current_book = list(bible_chapters.keys())[0]
+            current_chapter = 1
     
     return weekly_plan
+
 
 def patch_cell(cell, text):
     cell.text = ""
@@ -107,7 +115,9 @@ def main():
 
     start_book = os.getenv('START_BOOK')
     start_chapter = os.getenv('START_CHAPTER')
-    date = os.getenv('DATE')
+
+    date = get_formatted_date(os.getenv('DATE'))
+
     selected_custom = os.getenv('SELECTED_CUSTOM')
     
     weekly_plan = get_weekly_plan(start_book=start_book, start_chapter=start_chapter)
